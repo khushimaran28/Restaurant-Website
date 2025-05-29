8eb3f983adaf7d2c8a869103b5b159fe84d54834
khushimaran28 <150334758+khushimaran28@users.noreply.github.com>
2025-05-29 19:40:20 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import AboutUs from './AboutUs/AboutUs';
+import Chef from './Chef/Chef';
+import FindUs from './Findus/FindUs';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
+import Intro from './Intro/Intro';
+import Laurels from './Laurels/Laurels';
+import SpecialMenu from './Menu/SpecialMenu';
 
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+export {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+};
